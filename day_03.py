6efb777fdf3e5246a47c48d76c317243eeae8cf1
Phil Quinn <phil@philquinn.co.uk>
2024-12-03 16:01:33 +0000

    Day 03 - bit of golfin

--- day_03.py
+++ day_03.py
@@ -3,22 +3,19 @@ import re
 # Input
 input = open('data/day_03.txt').read()
 
-# Part 1
-print('Part 1')
-print(sum(int(a) * int(b) for a, b in re.findall(r'mul\((\d+),(\d+)\)', input)))
+for p in [1,2]:
+    print(f"Part {p}")
 
-# Part 2
-print('Part 2')
-do_mul = True
-total = 0
+    do_mul = True
+    total = 0
 
-for m in (re.finditer(r"(do\(\))|(don't\(\))|(mul)\((?P<a>\d+),(?P<b>\d+)\)", input)):
-    if m.group(0) =='do()':
-        do_mul = True
-    elif m.group(0) =="don't()":
-        do_mul = False
-    else:
-        total = total + (int(m.group('a')) * int(m.group('b')) * do_mul)
+    for m in (re.finditer(r"(do\(\))|(don't\(\))|(mul)\((?P<a>\d+),(?P<b>\d+)\)", input)):
+        if m.group(0) =='do()':
+            do_mul = True
+        elif m.group(0) =="don't()":
+            do_mul = False
+        else:
+            total += (int(m.group('a')) * int(m.group('b')) * (1 if p==1 else do_mul))
 
-print(total)
+    print(total)
 
